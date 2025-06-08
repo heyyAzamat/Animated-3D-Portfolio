fd8dac3e998bc94cd5b9b81e6eba07672007a6d3
heyyAzamat <azaamat.se@gmail.com>
2025-06-08 20:33:23 +0500

    commit

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 
 const Navbar = () => {
+  const[isOpen, setIsOpen] = useState(false);
   return <div className='fixed inset-x-0 z-20 w-full backdrop-blur-lg bg-primary/40'>
     <div className='mx-auto c-space max-w-7xl'>
         <div className='flex items-center justify-between py-2 sm:py-0'>
-            <a href="/" className='text-xl font-bold transition-colors text-neutral-400 hover:text-white'>
+            <a 
+              href="/" 
+              className='text-xl font-bold transition-colors text-neutral-400 hover:text-white'>
                 Azamat
             </a>
             <button>
-                <img src="" alt="" />
+                <img 
+                  src={isOpen ? "assets" : "assets/menu.svg"} 
+                  className="w-6 h-6" 
+                  alt="toggle"/>
             </button>
         </div>
     </div>
